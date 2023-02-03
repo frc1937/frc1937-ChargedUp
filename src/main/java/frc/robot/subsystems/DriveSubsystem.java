8c6c56766eac1682948ceff3bbf7f7526e1553fb
Liramy <95769845+Liramy@users.noreply.github.com>
2023-02-03 21:35:21 +0200

    changed names and added correct ports

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -5,6 +5,7 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.sensors.Pigeon2;
+import com.ctre.phoenix.sensors.WPI_Pigeon2;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
@@ -15,20 +16,22 @@ import frc.robot.Constants.Ports.*;
 
 public class DriveSubsystem extends SubsystemBase {
 
-  private final CANSparkMax left_motor_1 = new CANSparkMax(Drive.LEFT_MOTOR_1, MotorType.kBrushless);
-  private final CANSparkMax left_motor_2 = new CANSparkMax(Drive.LEFT_MOTOR_2, MotorType.kBrushless);
-  private final MotorControllerGroup m_left = new MotorControllerGroup(left_motor_1, left_motor_2);
+  private final CANSparkMax left_motor_front = new CANSparkMax(Drive.LEFT_MOTOR_FRONT, MotorType.kBrushless);
+  private final CANSparkMax left_motor_rear = new CANSparkMax(Drive.LEFT_MOTOR_REAR, MotorType.kBrushless);
+  private final MotorControllerGroup m_left = new MotorControllerGroup(left_motor_front, left_motor_rear);
 
-  private final CANSparkMax right_motor_1 = new CANSparkMax(Drive.RIGHT_MOTOR_1, MotorType.kBrushless);
-  private final CANSparkMax right_motor_2 = new CANSparkMax(Drive.RIGHT_MOTOR_2, MotorType.kBrushless);
-  private final MotorControllerGroup m_right = new MotorControllerGroup(right_motor_1, right_motor_2);
+  private final CANSparkMax right_motor_front = new CANSparkMax(Drive.RIGHT_MOTOR_FRONT, MotorType.kBrushless);
+  private final CANSparkMax right_motor_rear = new CANSparkMax(Drive.RIGHT_MOTOR_REAR, MotorType.kBrushless);
+  private final MotorControllerGroup m_right = new MotorControllerGroup(right_motor_front, right_motor_rear);
 
   // differential drive and pigeon
   private final DifferentialDrive m_drive = new DifferentialDrive(m_left, m_right);
-  private final Pigeon2 pigeon = new Pigeon2(0);
+  private final WPI_Pigeon2 pigeon = new WPI_Pigeon2(0);
 
   
-  public DriveSubsystem() {}
+  public DriveSubsystem() {
+    ResetPigeon();
+  }
 
   @Override
   public void periodic() {}
@@ -45,6 +48,16 @@ public class DriveSubsystem extends SubsystemBase {
   // Stop the motors dead in its tracks
   public void StopMotors() {
     m_drive.stopMotor();
+  }
+
+  // Reset the pigeon data
+  public void ResetPigeon() {
+    pigeon.reset();
+  }
+
+  // Get the current Yaw of the pigeon
+  public double GetYaw() {
+    return pigeon.getYaw();
   }
 
 
