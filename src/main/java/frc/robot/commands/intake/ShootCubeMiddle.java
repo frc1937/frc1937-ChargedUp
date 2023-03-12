cc9fe63b78fcc6b079b72b8c3e140baa5d9b7506
Liram <lirammash1@gmail.com>
2023-03-13 00:37:08 +0200

    One liner

--- src/main/java/frc/robot/commands/intake/ShootCubeMiddle.java
+++ src/main/java/frc/robot/commands/intake/ShootCubeMiddle.java
@@ -4,7 +4,6 @@
 
 package frc.robot.commands.intake;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.IntakeConstants;
 import frc.robot.subsystems.IntakeSubsystem;
